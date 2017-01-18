31d7a7eaaa3f7beceb02e54793364aab9bc9fa79
DELL <DELL@DELL-PC>
2017-01-18 08:50:46 +0000

    changement des fichiers

--- src/Projet/UserBundle/Controller/OuvrierController.php
+++ src/Projet/UserBundle/Controller/OuvrierController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Projet\UserBundle\Entity\Demande;
 use Projet\UserBundle\Entity\Ouvrier;
-
+use Projet\UserBundle\Entity\Tarif;
 class OuvrierController extends Controller
 {
      /**
@@ -20,5 +20,32 @@ class OuvrierController extends Controller
         $o = $em->getRepository('ProjetUserBundle:Ouvrier')->findAll();
         return new JsonResponse($o);
     }
-   
+    /**
+     * @Route("/agent_administratif/ouvriers")
+     * @Method({"POST"})
+     */
+    public function enregisterOuvrierAction(){
+           $em = $this->getDoctrine()->getManager();
+         $o = $request->get('ouvrier');
+         $ouvrier = json_decode($o);   
+         $ouv = new Ouvrier();
+         $ouv->setNom($ouvrier->nom);
+         $ouv->setPrenom($ouvrier->prenom);
+         $ouv->setAdresse($ouvrier->adresse);
+         $ouv->setTel($ouvrier->tel);
+         
+          foreach($ouvrier->tarifs as $t){
+              $tarif= new Tarif();
+             $prestation = $em->getRepository(Prestation::class)->find($t->prestation->id);
+             $tarif->setPrestation($prestation);
+             $tarif->setPrix($t->prix);
+           
+             $em->persist($tarif);
+             $ouv->addTarif($tarif);
+         }
+         
+         $em->persist($ouv);
+         $em->flush();
+         return new JsonResponse(true);
+    }
 }
