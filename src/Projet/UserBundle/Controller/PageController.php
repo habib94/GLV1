f955f7745c4b002a5dca00e261bddea463bb2f35
habib94 <elhabib.benahmed@gmail.com>
2017-01-18 00:02:50 +0000

    technique pages

---
+++ src/Projet/UserBundle/Controller/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Projet\UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('indexVisiteur.html.twig');
+    }
+
+    /**
+     * @Route("/agent_technique")
+     */
+    public function indexAgentTechniqueAction()
+    {
+        return $this->render('agentTechnique.html.twig');
+    }
+   
+    
+}
